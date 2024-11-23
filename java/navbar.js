ea46d10d6afe4894957f76cf983343e8a9f8bbf7
Aidan Hernandez <118579227+AidanHernandez@users.noreply.github.com>
2024-11-23 01:01:18 -0500

    fixing navbar, lots of issues fixed adding animations/transitions

--- java/navbar.js
+++ java/navbar.js
@@ -19,9 +19,8 @@ function hamburger_click() {
   document.getElementById("hamburger").classList.add('clicked')
   document.getElementById("models-submenu").classList.add('almost-clicked')
   document.getElementById("merch-submenu").classList.add('almost-clicked')
+  
 }
-
-
 
 
 
@@ -30,23 +29,54 @@ function hamburger_click() {
 
 // when the models is clicked
   function models_nav_click() {
-    document.getElementById("models-submenu").classList.add('clicked')
-    document.getElementById("models-submenu-blackwidow").classList.add('almost-clicked')
-    document.getElementById("models-submenu-nemesis").classList.add('almost-clicked')
-    document.getElementById("merch-submenu").classList.add('almost-clicked')
+    if(document.getElementById("merch-submenu").classList.contains("clicked")){
+
+      document.getElementById("merch-submenu").classList.add('back')
+      setTimeout(back_merch_removed, 1000)
+      document.getElementById("models-submenu").classList.add('clicked')
+      document.getElementById("models-submenu-blackwidow").classList.add('almost-clicked')
+      document.getElementById("models-submenu-nemesis").classList.add('almost-clicked')
+      document.getElementById("merch-submenu").classList.add('almost-clicked')
+
+    }
+    else{
+
+    
+      document.getElementById("models-submenu").classList.add('clicked')
+      document.getElementById("models-submenu-blackwidow").classList.add('almost-clicked')
+      document.getElementById("models-submenu-nemesis").classList.add('almost-clicked')
+      document.getElementById("merch-submenu").classList.add('almost-clicked')
+
+    }
   }
 
 
     //when blackwidow or nemesis is clicked
     function Black_Widow_nav_click() {
-      document.getElementById("models-submenu-blackwidow").classList.add('clicked')
+
+      if (document.getElementById("models-submenu-nemesis").classList.contains("clicked")){
+        document.getElementById("models-submenu-nemesis").classList.add('back')
+        setTimeout(back_nemesis_removed, 1000)
+        document.getElementById("models-submenu-blackwidow").classList.add('clicked')
+      }
+      else{
+        document.getElementById("models-submenu-blackwidow").classList.add('clicked')
+      }
+      
 
     }
 
 
     function nemesis_nav_click(){
+
+      if (document.getElementById("models-submenu-blackwidow").classList.contains("clicked")){
+        document.getElementById("models-submenu-blackwidow").classList.add("back")
+        setTimeout(back_blackwidow_removed, 1000)
+        document.getElementById("models-submenu-nemesis").classList.add('clicked')
+      }
+      else{
       document.getElementById("models-submenu-nemesis").classList.add('clicked')
-    
+      }
     }
 
 
@@ -56,14 +86,32 @@ function hamburger_click() {
 
   // returns to previouse state
     function back_models_submenu(){
-      document.getElementById("models-submenu").classList.add('back')
-      setTimeout(back_model_removed, 1000)
+
+      if(document.getElementById("models-submenu-blackwidow").classList.contains("clicked")){
+        document.getElementById("models-submenu-blackwidow").classList.add("models-blackwidow-back")
+        document.getElementById("models-submenu").classList.add('back')
+        setTimeout(all_models_close, 1000)
+
+      }
+      else if (document.getElementById("models-submenu-nemesis").classList.contains("clicked")){
+        document.getElementById("models-submenu-nemesis").classList.add("models-nemesis-back")
+        document.getElementById("models-submenu").classList.add('back')
+        setTimeout(all_models_close, 1000)
+
+      }
+      else{
+        document.getElementById("models-submenu").classList.add('back')
+        setTimeout(back_model_removed, 1000)
+      }
+
+    
     }
 
     function back_blackwidow() {
       document.getElementById("models-submenu-blackwidow").classList.add('back')
       setTimeout(back_blackwidow_removed, 1000)
     }
+
 
     function back_nemesis() {
       document.getElementById("models-submenu-nemesis").classList.add('back')
@@ -77,9 +125,6 @@ function hamburger_click() {
     function back_model_removed(){
       document.getElementById("models-submenu").classList.remove('back')
       document.getElementById("models-submenu").classList.remove('clicked')
-
-      document.getElementById("merch-submenu").classList.remove('back')
-      document.getElementById("merch-submenu").classList.remove('clicked')
     }
 
     function back_nemesis_removed(){
@@ -90,6 +135,7 @@ function hamburger_click() {
 
     function back_blackwidow_removed(){
       document.getElementById("models-submenu-blackwidow").classList.remove('back')
+      
       document.getElementById("models-submenu-blackwidow").classList.remove('clicked')
     }
 
@@ -98,11 +144,58 @@ function hamburger_click() {
       document.getElementById("merch-submenu").classList.remove('clicked')
     }
 
+
+    function all_models_close(){
+      document.getElementById("models-submenu-blackwidow").classList.remove('models-blackwidow-back')
+      document.getElementById("models-submenu-blackwidow").classList.remove('back')
+      document.getElementById("models-submenu-blackwidow").classList.remove('clicked')
+      document.getElementById("merch-submenu-blackwidow").classList.remove('almost-clicked')
+
+      document.getElementById("models-submenu-nemesis").classList.remove('models-nemesis-back')
+      document.getElementById("models-submenu-nemesis").classList.remove('back')
+      document.getElementById("models-submenu-nemesis").classList.remove('clicked')
+      document.getElementById("merch-submenu-nemesis").classList.remove('almost-clicked')
+      
+      document.getElementById("models-submenu").classList.remove('back')
+      document.getElementById("models-submenu").classList.remove('clicked')
+      document.getElementById("merch-submenu").classList.remove('almost-clicked')
+
+      document.getElementById("menu").classList.remove("exit")
+
+
+      document.getElementById("merch-submenu").classList.remove('back')
+      document.getElementById("merch-submenu").classList.remove('clicked')
+      document.getElementById("merch-submenu").classList.remove('almost-clicked')
+      document.getElementById("merch-submenu").classList.remove('exit')
+    }
   
 
 //merch clicked
 function merch_nav_click() {
-  document.getElementById("merch-submenu").classList.add('clicked')
+
+  if (document.getElementById("models-submenu").classList.contains("clicked")){
+    
+    if(document.getElementById("models-submenu-blackwidow").classList.contains("clicked")){
+      document.getElementById("models-submenu-blackwidow").classList.add("models-blackwidow-back")
+      document.getElementById("merch-submenu").classList.add('clicked')
+      setTimeout(all_models_close, 1000)
+    }
+    else if (document.getElementById("models-submenu-nemesis").classList.contains("clicked")){
+      document.getElementById("models-submenu-nemesis").classList.add("models-nemesis-back")
+      document.getElementById("merch-submenu").classList.add('clicked')
+      setTimeout(all_models_close, 1000)
+    }
+    else{
+      document.getElementById("models-submenu").classList.add('back')
+      setTimeout(back_model_removed, 1000)
+      document.getElementById("merch-submenu").classList.add('clicked')
+    }
+
+    
+  }
+  else{
+    document.getElementById("merch-submenu").classList.add('clicked')
+  }
 }
 
 
@@ -120,14 +213,56 @@ function back_merch_submenu(){
 
   //closes navbar
 function exit_nav_click() {
-  document.getElementById("hamburger").classList.remove('clicked')
 
-  document.getElementById("models-submenu").classList.remove('clicked')
-  document.getElementById("models-submenu").classList.remove('almost-clicked')
+  if(document.getElementById("models-submenu").classList.contains('clicked')){
 
-  document.getElementById("models-submenu-blackwidow").classList.remove('clicked')
-  document.getElementById("models-submenu-blackwidow").classList.remove('almost-clicked')
+    if(document.getElementById("models-submenu-blackwidow").classList.contains("clicked")){
+      document.getElementById("models-submenu-blackwidow").classList.add("models-blackwidow-back")
+      document.getElementById("models-submenu").classList.add("back")
+      document.getElementById("menu").classList.add("exit")
+      setTimeout(all_models_close, 1000)
+      setTimeout(() => document.getElementById("hamburger").classList.remove('clicked'), 1050)
+    }
+    else if (document.getElementById("models-submenu-nemesis").classList.contains("clicked")){
+      document.getElementById("models-submenu-nemesis").classList.add("exit")
+      document.getElementById("models-submenu").classList.add("back")
+      document.getElementById("menu").classList.add("exit")
+      setTimeout(all_models_close, 1000)
+      setTimeout(() => document.getElementById("hamburger").classList.remove('clicked'), 1050)
+      
+    }
+    else{
+      document.getElementById("models-submenu").classList.add("back")
+      document.getElementById("menu").classList.add("exit")
+      setTimeout(all_models_close, 1000)
+      setTimeout(() => document.getElementById("hamburger").classList.remove('clicked'), 6050)
+    }
 
-  document.getElementById("models-submenu-nemesis").classList.remove('clicked')
-  document.getElementById("models-submenu-nemesis").classList.remove('almost-clicked')
+  }
+  else if(document.getElementById("merch-submenu").classList.contains('clicked'))
+  {
+    
+    document.getElementById("merch-submenu").classList.add("exit")
+    
+    document.getElementById("menu").classList.add("exit")
+    setTimeout(all_models_close, 1000)
+    setTimeout(() => document.getElementById("hamburger").classList.remove('clicked'), 1050)
+    
+  }
+  
+  // document.getElementById("hamburger").classList.remove('clicked')
+
+  // document.getElementById("models-submenu").classList.remove('clicked')
+  // document.getElementById("models-submenu").classList.remove('almost-clicked')
+
+  // document.getElementById("models-submenu-blackwidow").classList.remove('clicked')
+  // document.getElementById("models-submenu-blackwidow").classList.remove('almost-clicked')
+
+  // document.getElementById("models-submenu-nemesis").classList.remove('clicked')
+  // document.getElementById("models-submenu-nemesis").classList.remove('almost-clicked')
+
+  // document.getElementById("merch-submenu").classList.remove('clicked')
+  // document.getElementById("merch-submenu").classList.remove('almost-clicked')
 }
+
+
