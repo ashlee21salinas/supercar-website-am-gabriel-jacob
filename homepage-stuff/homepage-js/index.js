ea76d97e05699628dc81e75c7ce4dd274131d561
Aidan Hernandez <118579227+AidanHernandez@users.noreply.github.com>
2024-12-04 00:20:15 -0500

    working on rest of homepage

--- homepage-stuff/homepage-js/index.js
+++ homepage-stuff/homepage-js/index.js
@@ -2,6 +2,8 @@ document.getElementById("index_carosel-option1").addEventListener("click", caros
 document.getElementById("index_carosel-option2").addEventListener("click", carosel_option2);
 document.getElementById("index_carosel-option3").addEventListener("click", carosel_option3);
 document.getElementById("index_carosel-option4").addEventListener("click", carosel_option4);
+document.getElementById("side-scroll-feature").addEventListener("click", side_feature_open);
+
 
 const option1 = document.getElementById("index_carosel-option1")
 const option2 = document.getElementById("index_carosel-option2")
@@ -10,6 +12,7 @@ const option4 = document.getElementById("index_carosel-option4")
 const hero = document.getElementById("hero")
 const blocker = document.getElementById("hero-blocker")
 
+side_feature_clicked = false
 
 function carosel_option1(){
     option1.classList.add("selected")
@@ -24,6 +27,8 @@ function carosel_option1(){
     setTimeout(() =>  document.getElementById("hero-text").style.marginBottom = "25%", 1500)
 
     setTimeout(() =>  document.getElementById("car-model").textContent = "Nemesis", 1500)
+
+    setTimeout(() => document.getElementById("hero-text").style.position = "absolute", 2000)
 }
 
 function carosel_option2(){
@@ -33,13 +38,15 @@ function carosel_option2(){
     option4.classList.remove("selected")
     
     document.getElementById("hero-text").style.position = "fixed"
-    setTimeout(() =>  hero.style.backgroundImage = "url(homepage-img/hero-img2-placeholder.jpg)", 2000)
+    
     blocker.style.animationName = "blocker-in"
     setTimeout(() =>  blocker.style.animationName = "blocker-out", 2000)
     setTimeout(() =>  document.getElementById("hero-text").style.left = "12.5%", 1500)
     setTimeout(() =>  document.getElementById("hero-text").style.marginBottom = "28.5%", 1500)
 
     setTimeout(() =>  document.getElementById("car-model").textContent = "BlackWidow", 1500)
+    setTimeout(() =>  hero.style.backgroundImage = "url(homepage-img/hero-img2-placeholder.jpg)", 2000)
+    setTimeout(() => document.getElementById("hero-text").style.position = "absolute", 2000)
 
     
 }
@@ -56,4 +63,16 @@ function carosel_option4(){
     option2.classList.remove("selected")
     option3.classList.remove("selected")
     option4.classList.add("selected")
+}
+
+function side_feature_open(){
+    if( side_feature_clicked == false)
+    {
+        document.getElementById("side-scroll-feature").style.backgroundImage = "url(homepage-img/side-view-maybe2.jpg)"
+        side_feature_clicked = true
+    }
+    else{
+        document.getElementById("side-scroll-feature").style.backgroundImage = "url(homepage-img/side-view-maybe.jpg)"
+        side_feature_clicked = false
+    }
 }
