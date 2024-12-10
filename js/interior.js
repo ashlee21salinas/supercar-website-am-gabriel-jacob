91bbff18605ff9ce75e5ef368825de426fe0aaf1
Aidan Hernandez <118579227+AidanHernandez@users.noreply.github.com>
2024-12-10 09:50:12 -0800

    confrim, interior, signup basically done

--- js/interior.js
+++ js/interior.js
@@ -2,20 +2,8 @@ document.getElementById("premium-options-arrow").addEventListener("click", premi
 
 document.getElementById("colorwheel-arrow").addEventListener("click", color_wheel_arrow);
 
-document.getElementById("3d").addEventListener("click", threed);
-document.getElementById("2d").addEventListener("click", twod);
 
-document.getElementById("img-1").addEventListener("click", one);
-document.getElementById("img-2").addEventListener("click", two);
-document.getElementById("img-3").addEventListener("click", three);
-document.getElementById("img-4").addEventListener("click", four);
-document.getElementById("img-5").addEventListener("click", five);
 
-var ones = document.getElementById("img-1")
-var twos = document.getElementById("img-2")
-var threes = document.getElementById("img-3")
-var fours = document.getElementById("img-4")
-var fives = document.getElementById("img-5")
 
 const green = document.getElementById("fancygreen-exterior")
 const blue = document.getElementById("fancyblue-exterior")
@@ -23,6 +11,12 @@ const red = document.getElementById("fancyred-exterior")
 const black = document.getElementById("fancyblack-exterior")
 
 
+
+
+green.addEventListener("click", green_click)
+blue.addEventListener("click", blue_click)
+red.addEventListener("click", red_click)
+black.addEventListener("click", black_click)
 
 
 function premium_arrow(){
@@ -102,111 +96,96 @@ function color_wheel_arrow(){
 }
 
 
-
-
-function threed(){
-    document.getElementById("3d").classList.remove("clicked")
-    document.getElementById("2d").classList.remove("clicked")
-
-    document.getElementById("3d").classList.add("clicked")
-
-
-    car = document.querySelectorAll('canvas')
-
-    car.forEach((canvas) => {
-        canvas.style.zIndex = "1";
-        canvas.style.left = "0"
-    });
-
-    document.getElementById("colorCanvas").style.left = ""
-    
-    
-}
+function green_click(){
+    green.classList.remove("clicked")
+    blue.classList.remove("clicked")
+    red.classList.remove("clicked")
+    black.classList.remove("clicked")
   
-function twod(){
-    document.getElementById("3d").classList.remove("clicked")
-    document.getElementById("2d").classList.remove("clicked")
-
-    document.getElementById("2d").classList.add("clicked")
-
-    car = document.querySelectorAll('canvas')
-
-    car.forEach((canvas) => {
-        canvas.style.zIndex = "0";
-    });
+    green.classList.add("clicked")
     
-    document.getElementById("colorCanvas").style.zIndex = "1"
-}
-
-function one(){
-    ones.classList.remove("selected")
-    twos.classList.remove("selected")
-    threes.classList.remove("selected")
-    fours.classList.remove("selected")
-    fives.classList.remove("selected")
-
-    ones.classList.add("selected")
-
-    document.getElementById("car-pic-exterior").style.backgroundImage = "url(../img/configurator-img/nemesis-img/front-black.png)"
-    document.getElementById("car-pic-exterior").style.backgroundPosition = "55% 50%"
+  }
+  
+  function blue_click(){
+    green.classList.remove("clicked")
+    blue.classList.remove("clicked")
+    red.classList.remove("clicked")
+    black.classList.remove("clicked")
+  
+    blue.classList.add("clicked")
     
-}
-
-function two(){
-    ones.classList.remove("selected")
-    twos.classList.remove("selected")
-    threes.classList.remove("selected")
-    fours.classList.remove("selected")
-    fives.classList.remove("selected")
-
-    twos.classList.add("selected")
-
-    document.getElementById("car-pic-exterior").style.backgroundImage = "url(../img/configurator-img/nemesis-img/threef-black.png)"
-    document.getElementById("car-pic-exterior").style.backgroundPosition = "55% 50%"
-
-}
-
-function three(){
-    ones.classList.remove("selected")
-    twos.classList.remove("selected")
-    threes.classList.remove("selected")
-    fours.classList.remove("selected")
-    fives.classList.remove("selected")
-
-    threes.classList.add("selected")
-
-    document.getElementById("car-pic-exterior").style.backgroundImage = "url(../img/configurator-img/nemesis-img/side-black.png)"
-
-    document.getElementById("car-pic-exterior").style.backgroundPosition = "35% 50%"
-    // background-position: 55% 50%;
-}
-
-function four(){
-    ones.classList.remove("selected")
-    twos.classList.remove("selected")
-    threes.classList.remove("selected")
-    fours.classList.remove("selected")
-    fives.classList.remove("selected")
-
-    fours.classList.add("selected")
-    document.getElementById("car-pic-exterior").style.backgroundImage = "url(../img/configurator-img/nemesis-img/side-back-black.png)"
-    document.getElementById("car-pic-exterior").style.backgroundPosition = "05% 50%"
-
-}
-
-function five(){
-    ones.classList.remove("selected")
-    twos.classList.remove("selected")
-    threes.classList.remove("selected")
-    fours.classList.remove("selected")
-    fives.classList.remove("selected")
-
-    fives.classList.add("selected")
-
-    document.getElementById("car-pic-exterior").style.backgroundImage = "url(../img/configurator-img/nemesis-img/back-black.png)"
-    document.getElementById("car-pic-exterior").style.backgroundPosition = "55% 40%"
-
-}
+    
+  }
+  
+  function red_click(){
+    green.classList.remove("clicked")
+    blue.classList.remove("clicked")
+    red.classList.remove("clicked")
+    black.classList.remove("clicked")
+  
+    red.classList.add("clicked")
+    
+  }
+  
+  function black_click(){
+    green.classList.remove("clicked")
+    blue.classList.remove("clicked")
+    red.classList.remove("clicked")
+    black.classList.remove("clicked")
+  
+    black.classList.add("clicked")
+    
+  }
 
 
 
+
+  function initColorPicker() {
+    var canvas = document.getElementById('colorCanvas');
+    var canvasContext = canvas.getContext('2d');
+  
+    let gradient = canvas.getContext('2d').createLinearGradient(0, 0, canvas.width, 0)
+    gradient.addColorStop(0, '#ff0000')
+    gradient.addColorStop(1 / 6, '#ffff00')
+    gradient.addColorStop((1 / 6) * 2, '#00ff00')
+    gradient.addColorStop((1 / 6) * 3, '#00ffff')
+    gradient.addColorStop((1 / 6) * 4, '#0000ff')
+    gradient.addColorStop((1 / 6) * 5, '#ff00ff')
+    gradient.addColorStop(1, '#ff0000')
+    canvas.getContext('2d').fillStyle = gradient
+    canvas.getContext('2d').fillRect(0, 0, canvas.width, canvas.height)
+  
+    gradient = canvas.getContext('2d').createLinearGradient(0, 0, 0, canvas.height)
+    gradient.addColorStop(0, 'rgba(255, 255, 255, 1)')
+    gradient.addColorStop(0.5, 'rgba(255, 255, 255, 0)')
+    gradient.addColorStop(1, 'rgba(255, 255, 255, 0)')
+    canvas.getContext('2d').fillStyle = gradient
+    canvas.getContext('2d').fillRect(0, 0, canvas.width, canvas.height)
+  
+    gradient = canvas.getContext('2d').createLinearGradient(0, 0, 0, canvas.height)
+    gradient.addColorStop(0, 'rgba(0, 0, 0, 0)')
+    gradient.addColorStop(0.5, 'rgba(0, 0, 0, 0)')
+    gradient.addColorStop(1, 'rgba(0, 0, 0, 1)')
+    canvas.getContext('2d').fillStyle = gradient
+    canvas.getContext('2d').fillRect(0, 0, canvas.width, canvas.height)
+  
+  
+    canvas.onclick = function(e) {
+        console.log()
+      var imgData = canvasContext.getImageData((e.offsetX / canvas.clientWidth) * canvas.width, (e.offsetY / canvas.clientHeight) * canvas.height, 1, 1)
+      var rgb = imgData.data;
+      var color = "rgb(" + rgb[0] + ", " + rgb[1] + ", " + rgb[2] +  ")";
+      console.log("%c" + color, "color:" + color)
+  
+    
+  
+      document.getElementById("rgb").style.color=color;
+      document.getElementById("rgb").textContent=color;
+  
+  
+      
+      
+    }
+  }
+
+  initColorPicker()
