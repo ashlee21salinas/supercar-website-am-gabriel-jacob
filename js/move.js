21350df73a8b310afdb8019a1fd743027532f24b
Aidan Hernandez <118579227+AidanHernandez@users.noreply.github.com>
2024-12-12 00:19:04 -0500

    translation done

---
+++ js/move.js
@@ -0,0 +1,3 @@
+<iframe id=":1.container" class="goog-te-banner-frame skiptranslate" frameborder="0" src="javascript:''" style="visibility: visible; top: calc(100% - 40px);">...</iframe>
+
+document.getElementById("html").style.height = "0"
